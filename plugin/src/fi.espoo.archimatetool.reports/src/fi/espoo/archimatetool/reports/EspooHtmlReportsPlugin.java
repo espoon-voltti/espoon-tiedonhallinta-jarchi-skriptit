bff6c0873d906ae1ddf04935886982a27605708f
Jani Mattsson <jpm@iki.fi>
2021-08-13 14:28:55 +0300

    Lisätty Espoon Archi html-raportointipluginin lähdekoodi

---
+++ plugin/src/fi.espoo.archimatetool.reports/src/fi/espoo/archimatetool/reports/EspooHtmlReportsPlugin.java
@@ -0,0 +1,110 @@
+/**
+ * This program and the accompanying materials
+ * are made available under the terms of the License
+ * which accompanies this distribution in the file LICENSE.txt
+ */
+package fi.espoo.archimatetool.reports;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+import com.archimatetool.editor.preferences.IPreferenceConstants;
+import com.archimatetool.editor.preferences.Preferences;
+import com.archimatetool.editor.utils.StringUtils;
+
+import fi.espoo.archimatetool.reports.preferences.IEspooPreferenceConstants;
+import fi.espoo.archimatetool.reports.html.HTMLReportExporter;
+
+/**
+ * Activator
+ * 
+ * @author Phillip Beauvoir
+ * @author Jani Mattsson  (modifications for Espoo)
+ */
+public class EspooHtmlReportsPlugin extends AbstractUIPlugin {
+    
+    public static final String PLUGIN_ID = "fi.espoo.archimatetool.reports"; //$NON-NLS-1$
+
+    /**
+     * The shared instance
+     */
+    public static EspooHtmlReportsPlugin INSTANCE;
+
+    /**
+     * The File location of this plugin folder
+     */
+    private static File fPluginFolder;
+
+    public EspooHtmlReportsPlugin() {
+        INSTANCE = this;
+    }
+    
+    @Override
+    public void stop(BundleContext context) throws Exception {
+        try {
+            HTMLReportExporter.cleanPreviewFiles();
+        }
+        catch(Exception ex) {
+            ex.printStackTrace();
+        }
+        finally {
+            super.stop(context);
+        }
+    }
+
+    /**
+     * @return The plugins folder
+     */
+    public File getTemplatesFolder() {
+    	return getUserTemplatesFolder();
+        // return new File(getPluginFolder(), "templates"); //$NON-NLS-1$
+    }
+        
+    /**
+     * @return User-set Espoo user template folder
+     */
+    public File getUserTemplatesFolder() {
+        String s = getPreferenceStore().getString(IEspooPreferenceConstants.ESPOO_USER_REPORTS_FOLDER);
+        if(StringUtils.isSetAfterTrim(s)) {
+            File f = new File(s);
+            f.mkdirs();
+            if(f.exists() && f.isDirectory()) {
+                return f;
+            }
+        }
+        
+        return getDefaultUserTemplatesFolder();
+    }
+
+    /**
+     * @return Default Espoo user template folder
+     */
+    public File getDefaultUserTemplatesFolder() {
+        File folder = new File(Preferences.STORE.getString(IPreferenceConstants.USER_DATA_FOLDER), "espoo-reports"); //$NON-NLS-1$
+        folder.mkdirs();
+        return folder;
+    }
+
+    /**
+     * @return The File Location of this plugin
+     */
+    public File getPluginFolder() {
+        if(fPluginFolder == null) {
+            URL url = getBundle().getEntry("/"); //$NON-NLS-1$
+            try {
+                url = FileLocator.resolve(url);
+            }
+            catch(IOException ex) {
+                ex.printStackTrace();
+            }
+            fPluginFolder = new File(url.getPath());
+        }
+        
+        return fPluginFolder;
+    }
+}
---
+++ plugin/src/fi.espoo.archimatetool.reports/src/fi/espoo/archimatetool/reports/html/PreviewHTMLReportHandler.java
@@ -0,0 +1,45 @@
+/**
+ * This program and the accompanying materials
+ * are made available under the terms of the License
+ * which accompanies this distribution in the file LICENSE.txt
+ */
+package fi.espoo.archimatetool.reports.html;
+
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.jface.dialogs.MessageDialog;
+
+import com.archimatetool.editor.Logger;
+import com.archimatetool.editor.actions.AbstractModelSelectionHandler;
+import com.archimatetool.model.IArchimateModel;
+
+
+
+/**
+ * Command Action Handler for Preview HTML Report
+ * 
+ * @author Phillip Beauvoir
+ */
+public class PreviewHTMLReportHandler extends AbstractModelSelectionHandler {
+    
+    @Override
+    public Object execute(ExecutionEvent event) throws ExecutionException {
+        IArchimateModel model = getActiveArchimateModel();
+        if(model != null) {
+            try {
+                HTMLReportExporter exporter = new HTMLReportExporter(model);
+                exporter.preview();
+            }
+            catch(Exception ex) {
+                Logger.log(IStatus.ERROR, "Error saving HTML Report", ex); //$NON-NLS-1$
+                MessageDialog.openError(workbenchWindow.getShell(),
+                        Messages.HTMLReportAction_0,
+                        (ex.getMessage() == null ? ex.toString() : ex.getMessage()) );
+            }
+        }
+
+        return null;
+    }
+        
+}
